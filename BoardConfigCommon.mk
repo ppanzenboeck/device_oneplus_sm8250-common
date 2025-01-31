aeae1b11c60da1da5ea0b7d603b532968cc3d947
ppanzenboeck <panz.development@gmail.com>
2025-01-31 22:54:19 +0100

    use clang r49

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -104,6 +104,7 @@ BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := BRAND_SHOW_FLAG=oneplus
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
 TARGET_KERNEL_CONFIG := vendor/meteoric_defconfig
+TARGET_KERNEL_CLANG_VERSION := r498229b
 
 # Lineage Health
 TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/mmi_charging_enable
