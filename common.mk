35090c88e4f4e71a01c4b18150bb62d636cd3c3d
ppanzenboeck <panz.development@gmail.com>
2025-07-15 17:10:55 +0200

    move oos cam oplus-fwk libs to hardware/oplus [1/2]

--- common.mk
+++ common.mk
@@ -96,7 +96,7 @@ $(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+    android.hardware.camera.provider@2.4-service_64 \
     libcamera_metadata_shim \
     libcamera2ndk_vendor \
     libstdc++_vendor \
@@ -254,12 +254,6 @@ PRODUCT_PACKAGES += \
     OPlusSystemUIResCommon \
     WifiResTarget
 
-# Oplus-fwk
-PRODUCT_PACKAGES += \
-    oplus-fwk
-
-PRODUCT_BOOT_JARS += \
-    oplus-fwk
 
 # Partitions
 PRODUCT_PACKAGES += \
