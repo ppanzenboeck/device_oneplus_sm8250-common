abe6f4942531d837f6cfdf5baacf056377dddc7a
ppanzenboeck <panz.development@gmail.com>
2025-07-31 19:58:40 +0200

    Revert "sm8250-common: Switch to NCM USB tethering"
    This reverts commit a9608224f1e23399f5f8b71c365be468805942ed.

--- common.mk
+++ common.mk
@@ -248,7 +248,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResTarget \
-    NcmTetheringOverlay \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
     OPlusSystemUIResCommon \
