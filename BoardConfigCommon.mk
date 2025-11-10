a4974fccafc2b13f9bff44109535ec29ef17d084
ppanzenboeck <panz.development@gmail.com>
2025-11-10 08:58:45 +0100

    fixup! GameBar: include if-exists

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -98,6 +98,12 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := BRAND_SHOW_FLAG=oneplus
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
+
+# Lineage
+#TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig vendor/oplus.config
+
+
+# Meteoric
 TARGET_KERNEL_CONFIG := vendor/meteoric_defconfig
 TARGET_KERNEL_CLANG_VERSION := r498229b
 
@@ -177,3 +183,5 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/sm8250-common/BoardConfigVendor.mk
+
+SELINUX_IGNORE_NEVERALLOWS := true
